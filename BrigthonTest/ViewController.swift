e13ac5811268b3e04d5a16cb21f6a24767eeaf5a
Jeremy Endratno <jeremyendratno@gmail.com>
2021-12-28 09:00:12 +0700

    Initial Commit

---
+++ BrigthonTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrigthonTest
+//
+//  Created by Jeremy Endratno on 12/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
